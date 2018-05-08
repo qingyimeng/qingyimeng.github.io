2cf3a7c36a14f6df4e824b1f7d6962f3cf6fb540
ccy <ccy@mygxt.com>
2018-05-08 09:15:53 +0800

    Site updated: 2018-05-08 09:15:52

--- js/common/qrcode.js
+++ js/common/qrcode.js
@@ -1,20 +1,4 @@
-	$.fn.formValid = function () {
-		return $(this).valid({
-			errorPlacement: function (error, element) {
-				element.parents('.formValue').addClass('has-error');
-				element.parents('.has-error').find('i.error').remove();
-				element.parents('.has-error').append('<i class="form-control-feedback fa fa-exclamation-circle error" data-placement="left" data-toggle="tooltip" title="' + error + '"></i>');
-				$("[data-toggle='tooltip']").tooltip();
-				if (element.parents('.input-group').hasClass('input-group')) {
-					element.parents('.has-error').find('i.error').css('right', '33px')
-				}
-			},
-			success: function (element) {
-				element.parents('.has-error').find('i.error').remove();
-				element.parent().removeClass('has-error');
-			}
-		});
-	}
+
     $(function () {
         $('#qrcodeTable').qrcode({
             render: "table",
@@ -24,9 +8,6 @@
         });
     });
     function createqrcode() {
-        if (!$('#form1').formValid()) {
-            return false;
-        }
         $("#qrcodeTable").html("");
         $('#qrcodeTable').qrcode({
             render: "table",
